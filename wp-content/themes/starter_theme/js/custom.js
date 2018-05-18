5b0d40ed4cbbbf1eccc190326bc2b86b55a1d552
mrkariox <karol.bartkowski9@gmail.com>
2018-05-18 15:29:38 +0200

    abillity to make menu sticky, menu logo center/left choose abbility, menu togge button added basic styling, mixin placeholder color added

--- wp-content/themes/starter_theme/js/custom.js
+++ wp-content/themes/starter_theme/js/custom.js
@@ -24,19 +24,46 @@
 
 	}); //document ready
 
-
+	$(window).on('scroll', function() {
+		// stickyNav();
+	});
 
 	window.onload = function() {
-
-
 
 	}; //window onload
 
 	$( window ).resize(function() {
 
-
-
 	}); //window resize
+
+	function fixedMenuHeightMargin() {
+		$navbar = $('.navbar-starter-theme');
+
+		if (window.innerWidth > 767) {
+			var $menuHeight = $navbar.height();
+		} else {
+			var $menuHeight = $('.navbar-header').outerHeight();
+		}
+	
+		$('.navSpaceHolder').css('margin-bottom', $menuHeight);
+
+    }
+
+    function stickyNav() {
+        var $navbar = $('.navbar-starter-theme');
+        var $navbarHolder = $('.navSpaceHolder');
+        if ($(window).scrollTop() >= $navbarHolder.offset().top) {
+			if (!$navbar.hasClass('sticky')) {
+				fixedMenuHeightMargin();
+				$navbar.addClass('sticky');
+			}
+        } else {
+            if ($navbar.hasClass('sticky')) {
+				$('.navSpaceHolder').css('margin-bottom', 0);
+				$navbar.removeClass('sticky').trigger('stickyRemove');
+			}
+        }
+    }
 
 
 })(jQuery);
