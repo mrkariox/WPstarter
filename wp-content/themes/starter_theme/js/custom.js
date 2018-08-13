1fee6f9cc5014d0dd1a80cf8f329d64ea9bf7414
mrkariox <karol.bartkowski9@gmail.com>
2018-08-13 14:22:45 +0200

    add back to top btn

--- wp-content/themes/starter_theme/js/custom.js
+++ wp-content/themes/starter_theme/js/custom.js
@@ -27,6 +27,33 @@
 			$('.langSwitcher__list').slideToggle();
 		});
 
+		// ------------------------------------- BACK TO TOP --------------------------------
+		// browser window scroll (in pixels) after which the "back to top" link is shown
+			var offset = 300,
+			//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+			offset_opacity = 1200,
+			//duration of the top scrolling animation (in ms)
+			scroll_top_duration = 700,
+			//grab the "back to top" link
+			$back_to_top = $('.cd-top');
+
+		//hide or show the "back to top" link
+		$(window).scroll(function(){
+			( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+			if( $(this).scrollTop() > offset_opacity ) { 
+				$back_to_top.addClass('cd-fade-out');
+			}
+		});
+
+		//smooth scroll to top
+		$back_to_top.on('click', function(event){
+			event.preventDefault();
+			$('body,html').animate({
+				scrollTop: 0 ,
+				}, scroll_top_duration
+			);
+		});
+
 	}); //document ready
 
 	$(window).on('scroll', function() {
@@ -68,7 +95,7 @@
 				$navbar.removeClass('sticky').trigger('stickyRemove');
 			}
         }
-    }
+	}
 
 
 })(jQuery);
